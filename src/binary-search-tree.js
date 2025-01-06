9919e67199210f0a70deb1110ea08206eeb1e68f
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-01-06 20:38:33 +0300

    feat: solve +1 task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,30 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  Root = null;
+  //Root = new Node(null);
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.Root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let currentNode = this.Root;
+    if ( currentNode === null) {
+      currentNode = new Node(data)
+    } else {
+      if (data > currentNode.data) {
+        
+      } else {
+
+      }
+    }
   }
 
   has(/* data */) {
